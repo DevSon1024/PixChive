b16ee9d8303c8a336fa3c6f8394f4307d84c1d8d
Devendra Sonawane <dpsonawane789@gmail.com>
2026-04-28 18:45:09 +0530

    Updated to verison 1.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,8 +28,8 @@ android {
         applicationId = "com.devson.pixchive"
         minSdk = 24
         targetSdk = 36
-        versionCode = 13
-        versionName = "1.3"
+        versionCode = 140
+        versionName = "1.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
